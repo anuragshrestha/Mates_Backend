671c8f814e305de50a9c25fe39884e7752a8d581
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-07 21:57:00 -0600

    fetched likes and comments count for each post in batch from DynamoDB

--- models/homeFeedModel.js
+++ models/homeFeedModel.js
@@ -1,6 +1,5 @@
 const pool = require('../database/db');
-const {GetItemCommand, DynamoDBClient} = require('@aws-sdk/client-dynamodb');
-const {GetObjectCommand, S3Client, S3} = require('@aws-sdk/client-s3');
+const {BatchGetItemCommand, DynamoDBClient} = require('@aws-sdk/client-dynamodb');
 const getUserEmail = require('../utils/getCognitoUserEmail');
 
 
@@ -21,15 +20,8 @@ const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 
-//Initialize AWS S3 Client
-const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY
-    }
-});
 
+const TABLE_NAME = process.env.AWS_DYNAMODB_TABLE_NAME;
 
 //initialize dynamodb client
 const dynamoDBClient = new DynamoDBClient({
@@ -69,8 +61,10 @@ const getAllFollowees = async(user_id) => {
 };
 
 /**
- * query latest 20 posts including the poster full name and image_url from posts 
- * table based on the followee_id list and university_name.
+ * Query latest 20 posts including the poster full name and image_url from posts 
+ * table based on the followee_id list and university_name. 
+ * Then fetch the likes and comments count for each post, merge it and returns 
+ * all the 20 posts
 */
 
 
@@ -82,7 +76,7 @@ const getPosts = async(followee_ids, university_name) => {
 
     //query all the post and the full name, image url if the poster user id matches with the user id in users table
     let query = `
-     SELECT p.*, u.full_name, u.image_url
+     SELECT p.*, u.full_name, u.profile_image_url
      FROM posts p
      JOIN users u ON p.user_id = u.user_id 
     `;
@@ -110,20 +104,69 @@ const getPosts = async(followee_ids, university_name) => {
 
     const result = await pool.query(query, values);
 
-    return result.rows;
+    const posts = result.rows;
+
+    //extracts the post ids only
+    const postIds = posts.map(post => post.post_id);
 
 
+    //fetch all the likes and comments for each post
+    const fetchLikesComments = await getPostStatsFromDynamoDB(postIds);
+
+
+    //merge the likes and comments with its post and returns it
+    const mergePost = posts.map(post => ({
+       ...post,
+       likes: fetchLikesComments[post.post_id]?.likes || 0,
+       comments: fetchLikesComments[post.post_id]?.comments || 0
+    }));
+
+     console.log(JSON.stringify(mergePost, null, 2));
+    return mergePost;
 }
 
 
 
 
+//fetches all the likes and comments count for each postId.
+const getPostStatsFromDynamoDB = async (postIds) => {
+    if (postIds.length === 0) return {};
 
+    const keys = postIds.map(id => ({ post_id: { S: id } }));
 
+    const params = {
+        RequestItems: {
+            [TABLE_NAME]: {
+                Keys: keys,
+                ProjectionExpression: "post_id, comments, likes"
+            }
+        }
+    };
+
+    try {
+        const command = new BatchGetItemCommand(params);
+        const response = await dynamoDBClient.send(command);
+
+        const stats = {};
+        for (const item of response.Responses[TABLE_NAME]) {
+            const postId = item.post_id.S;
+            stats[postId] = {
+                likes: item.likes ? parseInt(item.likes.N) : 0,
+                comments: item.comments ? parseInt(item.comments.N) : 0
+            };
+        }
+
+        return stats;
+    } catch (error) {
+        console.error("Error fetching DynamoDB stats:", error);
+        return {};
+    }
+};
 
 
 
 module.exports = {
     userData,
-    getAllFollowees
+    getAllFollowees,
+    getPosts
 }
