18635e98db8a0907b07e5f355715dfd2d6fffe7e
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-21 16:03:29 -0600

    route for like/unlike post

--- routes/homeFeedRoute.js
+++ routes/homeFeedRoute.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router();
 const {verifyJWT, jwtVerifier} = require('../middlewares/verifyJWT');
-const {getFeed} = require('../controllers/homeFeedController');
+const {getFeed, likePost, unLikePost} = require('../controllers/homeFeedController');
 
 
+//route to fetch posts for Around you Screen
 router.get('/aroundyou-feed', verifyJWT(jwtVerifier), getFeed);
+
+//route to like a post
+router.post('/like-post',verifyJWT(jwtVerifier), likePost);
+
+
+//route to unlike a post
+router.delete('/unlike-post', verifyJWT(jwtVerifier), unLikePost);
+
+
+
 
 module.exports = router;
