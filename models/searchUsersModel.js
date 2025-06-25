1ef67bed9c381496ab7afec9b479eab6191e28f2
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-24 22:40:34 -0600

    query users full_name bases on the text

---
+++ models/searchUsersModel.js
@@ -0,0 +1,41 @@
+const pool = require('../database/db');
+
+require('dotenv').config();
+
+const searchUser = async(name) => {
+
+    const text = name.toLowerCase();
+
+    
+      const result = await pool.query(
+        `SELECT *, 
+          CASE
+           WHEN full_name ILIKE $1 || '%' OR full_name % $1 THEN similarity(full_name, $1)
+           ELSE similarity(university_name, $1)
+          END AS score
+        FROM users
+        WHERE (
+         full_name ILIKE $1 || '%' OR full_name % $1 OR
+         university_name ILIKE $1 || '%' OR university_name % $1
+        )
+         AND (
+          similarity(full_name, $1) > 0.2 OR
+          similarity(university_name, $1) > 0.2
+         )
+        ORDER BY
+         CASE
+          WHEN full_name ILIKE $1 || '%' OR university_name ILIKE $1 || '%' THEN 0
+          ELSE 1
+         END,
+         score DESC
+         LIMIT 20;
+        `,
+        [text]
+      );
+
+      return result.rows;
+}
+
+module.exports = {
+    searchUser
+}
