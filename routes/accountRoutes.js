be01e0c9c7105a43afb1857f1bcf0a12de1ff16d
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-04 02:49:24 -0600

    added route for help and support

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {verifyJWT, jwtVerifier} = require('../middlewares/verifyJWT');
 const {getUserPost, updateUserProfile} = require('../controllers/accountController');
 const {sendFeedback} = require("../controllers/feedbackController");
+const {sendHelpEmail} = require("../controllers/helpSupportController");
 const upload = require('../middlewares/uploadImage')
 
 
@@ -16,5 +17,7 @@ router.put('/account/user-profile', verifyJWT(jwtVerifier), upload.single('image
 
 //route to send feedback to the mates
 router.post('/account/feedback', verifyJWT(jwtVerifier), sendFeedback);
+
+router.post('/account/help-support', verifyJWT(jwtVerifier), sendHelpEmail);
 
 module.exports = router;
