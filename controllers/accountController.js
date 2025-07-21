1448260275dd0641a9fdfb53e768e67aa05bf3fe
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-07-20 20:04:01 -0600

    added bio field

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,10 @@
-const { fetchUserPost, updateUserData, fetchUserData, fetchUserCounts } = require("../models/accountModel");
-const redisClient = require('../utils/redis');
-
+const {
+  fetchUserPost,
+  updateUserData,
+  fetchUserData,
+  fetchUserCounts,
+} = require("../models/accountModel");
+const redisClient = require("../utils/redis");
 
 /**
  * Fetches the posts of the user based on the user_id
@@ -13,7 +17,6 @@ const getUserInfo = async (req, res) => {
   const limit = parseInt(req.query.limit) || 2;
   const offset = parseInt(req.query.offset) || 0;
 
-
   if (!userId) {
     return res.status(400).json({
       success: false,
@@ -21,84 +24,90 @@ const getUserInfo = async (req, res) => {
     });
   }
 
-
-     const userKey = `user:${userId}`;
-     let userData;
-     let fetchedUserDatapromise;
-     let shouldCache = false;
+  const userKey = `user:${userId}`;
+  let userData;
+  let fetchedUserDatapromise;
+  let shouldCache = false;
 
   try {
-
     const cachedUser = await redisClient.get(userKey);
 
     //checks if the user data is cached
-    if(cachedUser){
+    if (cachedUser) {
       const cachedUserData = JSON.parse(cachedUser);
       fetchedUserDatapromise = Promise.resolve(cachedUserData);
-    }else{
-      fetchedUserDatapromise =  fetchUserData(userId);
-      shouldCache = true
+    } else {
+      fetchedUserDatapromise = fetchUserData(userId);
+      shouldCache = true;
     }
 
     let [userData, userCounts, posts] = await Promise.all([
       fetchedUserDatapromise,
       fetchUserCounts(userId),
-      fetchUserPost(userId, limit, offset)
+      fetchUserPost(userId, limit, offset),
     ]);
 
     if (shouldCache) {
-      console.log('caching the user account profile data');
-      
-      await redisClient.set(userKey, JSON.stringify(userData), 'EX', 604800);
+      console.log("caching the user account profile data");
+
+      await redisClient.set(userKey, JSON.stringify(userData), "EX", 604800);
     }
 
-    console.log('user counts: ', userCounts);
-    
-      // combine them here
-      const userProfile = {
-        ...userData,
-        ...userCounts
-      };
+    console.log("user counts: ", userCounts);
 
-    posts = posts.map(post => {
-      try {
-        return {
-          ...post,
-          media_urls: JSON.parse(post.media_urls)
-        };
-      } catch (e) {
-        console.error(`Failed to parse media_urls for post ${post.post_id}:`, post.media_urls);
-        return null;
-      }
-    }).filter(Boolean); 
-       
-      console.log("posts:", JSON.stringify(posts, null, 2));
-      
-     console.log("successfully fetched user account data: ", userProfile);
-     
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully fetched post",
-        userProfile: userProfile,
-        posts: posts,
-      });
+    // combine them here
+    const userProfile = {
+      ...userData,
+      ...userCounts,
+    };
+
+    posts = posts
+      .map((post) => {
+        try {
+          return {
+            ...post,
+            media_urls: JSON.parse(post.media_urls),
+          };
+        } catch (e) {
+          console.error(
+            `Failed to parse media_urls for post ${post.post_id}:`,
+            post.media_urls
+          );
+          return null;
+        }
+      })
+      .filter(Boolean);
+
+    console.log("posts:", JSON.stringify(posts, null, 2));
+
+    console.log("successfully fetched user account data: ", userProfile);
+
+    return res.status(200).json({
+      success: true,
+      message: "Successfully fetched post",
+      userProfile: userProfile,
+      posts: posts,
+    });
   } catch (error) {
     console.log("failed to fetched posts: ", error.message);
     return res.status(500).json({ success: false, error: error.message });
   }
 };
 
-
+/**
+ * Checks if the field is non empty and then
+ * calls the model to update the data
+ * @param {*} req the data to be updated
+ * @param {*} res
+ * @returns success: True or False
+ */
 
 const updateUserProfile = async (req, res) => {
-
   const user_id = req.user?.username;
 
   const file = req.file;
 
-  const { full_name, major, school_year} = req.body;
+  const { full_name, bio, major, school_year } = req.body;
 
   const allowedTypes = [
     "image/jpeg",
@@ -117,31 +126,40 @@ const updateUserProfile = async (req, res) => {
     return res.status(400).json({ success: false, error: "empty field" });
   }
 
-  if(!file) {
-    return res.status(400).json({success: false, error: 'Profile image is required'});
-  }
-
-  if(!allowedTypes.includes(file.mimetype)){
-    return res.status(400).json({success: false, error: "unsupported image type"})
+  if (file) {
+    if (!allowedTypes.includes(file.mimetype)) {
+      return res
+        .status(400)
+        .json({ success: false, error: "unsupported image type" });
+    }
   }
 
   try {
     const result = await updateUserData(
       user_id,
       full_name,
+      bio,
       major,
       school_year,
       file
     );
 
     console.log("successfully updated user profile data");
-    
-    return res
-      .status(200)
-      .json({
-        success: true,
-        message: "successfully updated user profile data",
-      });
+
+    const userKey = `user:${user_id}`;
+
+    try {
+      await redisClient.del(userKey);
+      console.log('successfully deleted the user profile cache');
+      
+    } catch (err) {
+      console.log(`failed to delete the cache: ${err}`);
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "successfully updated user profile data",
+    });
   } catch (error) {
     console.log(`failed to update user profile data: ${error.message}`);
     return res.status(500).json({ success: false, error: error.message });
