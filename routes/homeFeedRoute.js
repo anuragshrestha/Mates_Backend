d60f99ad592a3aae499bca01904e5a82dda5e90f
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-08-19 23:28:27 -0600

    added route for `for you feed`

--- routes/homeFeedRoute.js
+++ routes/homeFeedRoute.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const router = express.Router();
 const {verifyJWT, jwtVerifier} = require('../middlewares/verifyJWT');
-const {getFeed, likePost, unLikePost} = require('../controllers/homeFeedController');
+const {getAroundYouFeed, getForYouFeed, likePost, unLikePost} = require('../controllers/homeFeedController');
+const { route } = require('./authRoutes');
 
 
 //route to fetch posts for Around you Screen
-router.get('/feeds/aroundyou', verifyJWT(jwtVerifier), getFeed);
+router.get('/feeds/aroundyou', verifyJWT(jwtVerifier), getAroundYouFeed);
+
+
+//route to fetch posts for For you Screen
+router.get('/feeds/foryou', verifyJWT(jwtVerifier), getForYouFeed);
 
 //route to like a post
 router.post('/posts/:postId/like',verifyJWT(jwtVerifier), likePost);
-
 
 //route to unlike a post
 router.delete('/posts/:postId/like', verifyJWT(jwtVerifier), unLikePost);
